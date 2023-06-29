945dd5181681d7bde00b7bbbe02fd118f9dcc4bb
Jose Antonio Rubio <jose.rubio@gambyt.com>
2023-06-29 11:05:06 -0400

    inserting dummy data  if there are no records

--- server/main.js
+++ server/main.js
@@ -1,20 +1,22 @@
-import _ from 'lodash'
-import { image, helpers } from '@faker-js/faker'
-import Meteor from 'meteor/meteor'
-import { Contacts } from '../imports/collections/contacts'
+import _ from "lodash";
+import { faker } from "@faker-js/faker";
+import { Meteor } from "meteor/meteor";
+import { Contacts } from "../imports/collections/contacts";
 
 Meteor.startup(() => {
-    const numberOfRecords = Contacts.find({}).count()
-    if(!numberOfRecords) {
-        _.times(5000, () => {
-            const { name, email, phone} = helpers.createCard()
+  const numberOfRecords = Contacts.find({}).count();
+  if (!numberOfRecords) {
+    _.times(5000, () => {
+      const name = faker.person.fullName();
+      const email = faker.internet.email();
+      const phone = faker.phone.number();
 
-            Contacts.insert({
-                name,
-                email,
-                phone,
-                avatar: image.avatar()
-            })
-        })
-    }
-})
+      Contacts.insert({
+        name,
+        email,
+        phone,
+        avatar: faker.image.avatar(),
+      });
+    });
+  }
+});
